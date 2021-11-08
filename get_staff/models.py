a52e98cf8cde347ab4ed3fb16ea1f8a59ca8aec9
Hyowon0211 <phw703@naver.com>
2021-11-08 19:54:46 +0900

    whitenoise

--- get_staff/models.py
+++ get_staff/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Post_g(models.Model):
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=20)
+    age = models.IntegerField()
