7b6dffcf41f65959ad3c4fa9c3ff1ca5df50dab4
Hyowon0211 <phw703@naver.com>
2021-11-08 23:19:13 +0900

    static file copied

--- ASAP_Prj/settings.py
+++ ASAP_Prj/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATIC_URL = '/static/'
